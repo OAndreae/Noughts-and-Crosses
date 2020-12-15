ca121ad67ee99d9d518120c78c058c780045a5ed
OAndreae <oliverandreae@outlook.com>
2020-12-15 20:44:39 +0000

    Removed commented out minimax class

--- Minimax.h
+++ Minimax.h
@@ -1,15 +1,4 @@
 #pragma once
 #include "Board.h"
 
-//class Minimax_player
-//{
-//public:
-//	Minimax_player(Player assigned_player) : m_player{assigned_player}{}
-//	Pos get_position(const Board& b);
-//private:
-//	Player m_player;
-//};
-
 Pos minimax_position(const Board& b);
-
-
